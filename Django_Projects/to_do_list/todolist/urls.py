e7f084c1da302257d08a137f2f68f0a82082417f
fred <244018719@qq.com>
2018-09-08 17:10:06 +0800

    解决网址名，网页名的冲突

--- Django_Projects/to_do_list/todolist/urls.py
+++ Django_Projects/to_do_list/todolist/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'todolist'
 urlpatterns = [
-    path('', views.home),
+    path('home/', views.home, name="主页"),
+    path('about/', views.about, name="关于"),
+    path('edit/', views.edit, name="编辑"),
 ]
--- Django_Projects/to_do_list/todolist/views.py
+++ Django_Projects/to_do_list/todolist/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'todolist/home.html')
+
+
+def about(request):
+    return render(request, 'todolist/about.html')
+
+
+def edit(request):
+    return render(request, 'todolist/edit.html')
