93dd311796b9fb220e9bfa8a5bee3b3d8d698343
fred <244018719@qq.com>
2018-09-13 21:48:35 +0800

    完成了增删改查

--- Django_Projects/to_do_list/todolist/urls.py
+++ Django_Projects/to_do_list/todolist/urls.py
@@ -5,5 +5,7 @@ app_name = 'todolist'
 urlpatterns = [
     path('home/', views.home, name="主页"),
     path('about/', views.about, name="关于"),
-    path('edit/', views.edit, name="编辑"),
+    path('edit/<forloop_counter>', views.edit, name="编辑"),
+    path('delete/<forloop_counter>', views.delete, name="删除"),
+    path('cross/<forloop_counter>', views.cross, name="划掉"),
 ]
--- Django_Projects/to_do_list/todolist/views.py
+++ Django_Projects/to_do_list/todolist/views.py
@@ -1,13 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
-# Create your views here.
+
+lst = [
+    {'待办事项': '遛狗', '已完成': True},
+]
+
+
 def home(request):
-    return render(request, 'todolist/home.html')
+    global lst
+    if request.method == "POST":
+        if request.POST['待办事项'] == '':
+            return render(request, 'todolist/home.html', {'警告': '请输入内容！'})
+        else:
+            lst.append({'待办事项': request.POST['待办事项'], '已完成': False})
+            content = { '清单': lst }
+            return render(request, 'todolist/home.html', content)
+
+    elif request.method == 'GET':
+        content = {'清单': lst}
+        return render(request, "todolist/home.html", content)
 
 
 def about(request):
     return render(request, 'todolist/about.html')
 
 
-def edit(request):
-    return render(request, 'todolist/edit.html')
+def edit(request, forloop_counter):
+    global lst
+    if request.method == "POST":
+        if request.POST['已修改事项'] == '':
+            return render(request, "todolist/edit.html", {'警告': '请输入内容！'})
+        else:
+            lst[int(forloop_counter) - 1]['待办事项'] = request.POST['已修改事项']
+            return redirect("todolist:主页")
+    elif request.method == "GET":
+        content = { '待修改事项': lst[int(forloop_counter) - 1]['待办事项'] }
+    return render(request, 'todolist/edit.html', content)
+
+
+
+def delete(request, forloop_counter):
+    global lst
+    lst.pop(int(forloop_counter) - 1)
+    return redirect("todolist:主页")
+
+
+def cross(request, forloop_counter):
+    global lst
+    if request.POST['完成状态'] == '已完成':
+        lst[int(forloop_counter) - 1]['已完成'] = True
+        return redirect("todolist:主页")
+    else:
+        lst[int(forloop_counter) - 1]['已完成'] = False
+        return redirect("todolist:主页")
