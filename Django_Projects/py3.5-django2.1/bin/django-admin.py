c89255b4c25111bb30086a1ead9399131da873b8
AIM-1993 <244018719@qq.com>
2018-10-11 19:52:50 +0800

    对全站点使用Redis进行缓存

---
+++ Django_Projects/py3.5-django2.1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fred/GithubProjects/To_Do_List/Django_Projects/py3.5-django2.1/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
